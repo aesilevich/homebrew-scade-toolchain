5c7cd274849ba3800e8a53d5f969521bfb88e7d6
Alexandr Esilevich <esil0x@gmail.com>
2024-08-22 11:57:40 +0100

    updated sha hash

--- Casks/swift-android-toolchain@5.9.2.rb
+++ Casks/swift-android-toolchain@5.9.2.rb
@@ -1,6 +1,6 @@
 cask "swift-android-toolchain@5.9.2" do
   version "5.9.2-1"
-  sha256 "c87dbc6aff5411676a471e84905d69c671b62b93b1210bd95c9d776d087de95c"
+  sha256 "15ca8cbf2ed59ca919d4e41f13eec1977d172b971d77b51752ae7030c122ee7d"
 
   url "https://github.com/scade-platform/swift-android-toolchain/releases/download/5.9.2-1/swift-android-compiler-5.9.2-1.tar.bz2"
   name "swift-android-toolchain@5.9.2"
